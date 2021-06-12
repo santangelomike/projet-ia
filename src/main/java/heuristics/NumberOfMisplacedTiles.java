b26eb77fd567718da9fb706af74cf262534b11d1
Santangelo Mike <mike.santangelo@dauphine.eu>
2021-06-12 11:31:45 +0200

    number of misplaced tiles

---
+++ src/main/java/heuristics/NumberOfMisplacedTiles.java
@@ -0,0 +1,25 @@
+package heuristics;
+
+import game.Puzzle;
+
+public class NumberOfMisplacedTiles implements Heuristic {
+    public int evaluate(Puzzle p) {
+        int distance = 0;
+        int size = p.getSize();
+
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
+                int value = p.getPiece(row, column);
+                distance += (row != value / size || column != value % size) ? 1 : 0;
+            }
+        }
+
+        return distance;
+    }
+
+    public static void main(String[] args) {
+        Puzzle puzzle = new Puzzle(3);
+        System.out.println(puzzle);
+        System.out.println(new NumberOfMisplacedTiles().evaluate(puzzle));
+    }
+}
